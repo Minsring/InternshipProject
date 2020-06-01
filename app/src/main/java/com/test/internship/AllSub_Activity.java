68c4c613c5309efee01a17124bb8d89bf89de509
minsring <lwslwskms@gmail.com>
2020-06-01 22:01:15 +0900

    mapLayout 추가

--- app/src/main/java/com/test/internship/AllSub_Activity.java
+++ app/src/main/java/com/test/internship/AllSub_Activity.java
@@ -1,13 +1,34 @@
 package com.test.internship;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AllSub_Activity extends AppCompatActivity {
+    Button mapBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.all_sub);
+
+        mapBtn = findViewById(R.id.mapBtn);
+        mapBtn.setOnClickListener(listener);
     }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = null;
+            switch (v.getId()){
+                case R.id.mapBtn:
+                    intent = new Intent(getApplicationContext(), AllSub_Map.class);
+//                    startActivity(intent);
+                    break;
+            }
+            if(intent != null) startActivity(intent);    // 다른 처리 없다면 여기서 한번에 화면 전환
+        }
+    };
 }
---
+++ app/src/main/java/com/test/internship/AllSub_Map.java
@@ -0,0 +1,13 @@
+package com.test.internship;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllSub_Map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.allsub__map);
+    }
+}
