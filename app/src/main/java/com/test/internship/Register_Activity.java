29f6e336780e11d26458b88365c6d86973a08a59
eomsso <eomsso@naver.com>
2020-06-03 17:03:22 +0900

    팀2 보호자정보처리

--- app/src/main/java/com/test/internship/Register_Activity.java
+++ app/src/main/java/com/test/internship/Register_Activity.java
@@ -1,12 +1,50 @@
 package com.test.internship;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Register_Activity extends AppCompatActivity{
+import java.util.ArrayList;
+
+public class Register_Activity<listener> extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
     }
+    // 보호자가 입력한 정보를 받아서 변수에 넣는 부분
+    EditText idEdit_n = (EditText)findViewById(R.id.pName);
+    String value_n = idEdit_n.getText().toString();
+
+    EditText idEdit_p = (EditText)findViewById(R.id.pPhone);
+    String value_p = idEdit_p.getText().toString();
+
+    /// ArrayList에 보호자정보(이름, 번호) 넣는 부분
+    int index=0;
+    ArrayList<String> protectorName = new ArrayList<String>();
+    ArrayList<String> protectorPhone = new ArrayList<String>();
+
+    public void getProtectorInfo() {
+        protectorName.add(index,value_n);
+        protectorPhone.add(index,value_p);
+        index++;
+        return;
+    }
+
+    Button addInfo = findViewById(R.id.confirm);
+    addInfo.setOnClickListener(listener);
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.confirm:
+                   getProtectorInfo();
+                    break;
+            }
+        }
+    };
 }
