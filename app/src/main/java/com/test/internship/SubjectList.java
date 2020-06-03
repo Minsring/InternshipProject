d647a51963a4727dd00b7215e1f581ee4197aac7
minsring <lwslwskms@gmail.com>
2020-06-03 11:17:04 +0900

    1-4, 1-5 화면연결, 테스트하기

--- app/src/main/java/com/test/internship/SubjectList.java
+++ app/src/main/java/com/test/internship/SubjectList.java
@@ -3,8 +3,11 @@ package com.test.internship;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class SubjectList extends AppCompatActivity {
     Button mapBtn;
@@ -16,6 +19,23 @@ public class SubjectList extends AppCompatActivity {
 
         mapBtn = findViewById(R.id.mapBtn);
         mapBtn.setOnClickListener(listener);
+
+        // 리사이클러뷰, 레이아웃 매니저
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Adapter 세팅
+        Adapter adapter = new Adapter();
+        adapter.addItem(new HospitalInformation("민슬 병원", "9:00", "17:00", "민슬집"
+                                                        , "010-1234-5678", "진료과", "일요일"));
+        adapter.addItem(new HospitalInformation("희정 병원", "11:00", "20:00", "희정집"
+                , "010-1122-5566", "희정과", "토요일"));
+        adapter.addItem(new HospitalInformation("민옥 병원", "0:00", "24:00", "민옥집"
+                , "010-1234-1234", "민옥과", "매일"));
+
+        recyclerView.setAdapter(adapter);
     }
 
     View.OnClickListener listener = new View.OnClickListener() {
