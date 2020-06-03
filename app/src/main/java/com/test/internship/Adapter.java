f35de85bc62a19cdb152255ef57d428c3494e4d0
minsring <lwslwskms@gmail.com>
2020-06-03 11:02:24 +0900

    1-3 Adapter 클래스 작성 -> 리사이클러 뷰 이용

--- app/src/main/java/com/test/internship/Adapter.java
+++ app/src/main/java/com/test/internship/Adapter.java
@@ -1,4 +1,73 @@
 package com.test.internship;
 
-public class Adapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    ArrayList<HospitalInformation> items = new ArrayList<HospitalInformation>();
+
+    // item으로 만든 itemView를 담아두는 뷰 홀더
+    // 일단 필요한 객체의 수만 ViewHolder에 담아서 화면에 뿌려준다
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView itemHospitalName, itemSubject;
+        TextView itemDistance, itemOpenClosed;      // distance, openclosed 부분
+        public ViewHolder(View itemView){
+            super(itemView);
+            itemHospitalName = itemView.findViewById(R.id.itemHospitalName);
+            itemSubject = itemView.findViewById(R.id.itemDistance);
+            // distance, openclosed 부분
+            itemDistance = itemView.findViewById(R.id.itemDistance);
+            itemOpenClosed = itemView.findViewById(R.id.itemOpenClosed);
+        }
+        public void setItem(HospitalInformation hospital){
+            itemHospitalName.setText(hospital.getHospitalName());
+            itemSubject.setText(hospital.getSubject());
+            // distance, openclosed 부분
+            itemDistance.setText(hospital.getDistance());
+            itemOpenClosed.setText(hospital.getOpenClosed());
+        }
+    }
+
+    // 임시로 사용
+    public void addItem(HospitalInformation item){
+        items.add(item);
+    }
+    public HospitalInformation getItem(int position){
+        return items.get(position);
+    }
+    // 해당위치의 item을 변경하는 함수
+    public void setItem(int position, HospitalInformation item){
+        items.set(position, item);
+    }
+    // 리스트를 set
+    public void setItems(ArrayList<HospitalInformation> items){
+        this.items = items;
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    @NonNull
+    @Override       // ViewHolder객체가 생성될 때 자동으로 호출
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View itemView = inflater.inflate(R.layout.item_style, viewGroup, false);
+
+        // 새로 생성된 ViewHolder 객체를 뷰 객체를 담아서 리턴
+        return new ViewHolder(itemView);
+    }
+
+    @Override       // ViewHolder객체가 재사용될 때 자동으로 호출
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        HospitalInformation item = items.get(i);
+        viewHolder.setItem(item);
+    }
 }
--- app/src/main/java/com/test/internship/HospitalInformation.java
+++ app/src/main/java/com/test/internship/HospitalInformation.java
@@ -1,15 +1,18 @@
 package com.test.internship;
 
 public class HospitalInformation {
-    private String hospitalName;
-    private String openTime;
-    private String closedTime;
-    private String address;
-    private String callNumber;
-    private String subject;
-    private String openDay;
+    private String hospitalName = "병원이름";
+    private String openTime = "여는시간";
+    private String closedTime = "닫는시간";
+    private String address = "주소";
+    private String callNumber = "전화번호";
+    private String subject = "진료과목";
+    private String openDay = "여는요일";    // 요일마다 영업시간이 바뀐다면?
+    private String distance = "0";         // 목록에 표시할 거라면 판단하는 함수 필요
+    private String openClosed = "영업중";         // 목록에 표시할 거라면 판단하는 함수 필요
 
     // 생성자 -> 사용할지는 모르게씀
+    // 일단 distance와 openClosed는 제외하고 만듬
     public HospitalInformation(String hospitalName, String openTime, String closedTime, String address,
                                String callNumber, String subject, String openDay){
         this.hospitalName = hospitalName;
@@ -29,4 +32,12 @@ public class HospitalInformation {
     public String getCallNumber() { return callNumber; }
     public String getSubject() { return subject; }
     public String getOpenDay() { return openDay; }
+    public String getDistance() {
+        // TODO: 현재위치로 부터 병원과의 거리 구하기
+        return distance+"km";
+    }
+    public String getOpenClosed() {
+        // TODO: 영업중인지 아닌지 판단하기
+        return openClosed;
+    }
 }
