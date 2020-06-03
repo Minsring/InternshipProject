fa5538c831c9f4f72fa700ec77d92aad53f24708
minsring <lwslwskms@gmail.com>
2020-06-03 18:28:59 +0900

    윤모 받아랏

--- app/src/main/java/com/test/internship/Register_Activity.java
+++ app/src/main/java/com/test/internship/Register_Activity.java
@@ -9,26 +9,31 @@ import android.widget.EditText;
 import java.util.ArrayList;
 
 public class Register_Activity<listener> extends AppCompatActivity{
+    // 보호자가 입력한 정보를 받아서 변수에 넣는 부분
+    EditText idEdit_n;
+    String value_n;
+
+    EditText idEdit_p;
+    String value_p;
+
+    int index=0;
+    ArrayList<String> protectorName = new ArrayList<String>();
+    ArrayList<String> protectorPhone = new ArrayList<String>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
         Button addInfo;
         addInfo = findViewById(R.id.confirm);
+        idEdit_n = (EditText)findViewById(R.id.pName);
+        idEdit_p = (EditText)findViewById(R.id.pPhone);
+
         addInfo.setOnClickListener(listener);
     }
 
-    // 보호자가 입력한 정보를 받아서 변수에 넣는 부분
-    EditText idEdit_n = (EditText)findViewById(R.id.pName);
-    String value_n = idEdit_n.getText().toString();
-
-    EditText idEdit_p = (EditText)findViewById(R.id.pPhone);
-    String value_p = idEdit_p.getText().toString();
-
     /// ArrayList에 보호자정보(이름, 번호) 넣는 부분
-    int index=0;
-    ArrayList<String> protectorName = new ArrayList<String>();
-    ArrayList<String> protectorPhone = new ArrayList<String>();
     public void getProtectorInfo() {
         protectorName.add(index,value_n);
         protectorPhone.add(index,value_p);
@@ -41,6 +46,8 @@ public class Register_Activity<listener> extends AppCompatActivity{
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.confirm:
+                    value_n = idEdit_n.getText().toString();
+                    value_p = idEdit_p.getText().toString();
                     getProtectorInfo();
                     break;
             }
