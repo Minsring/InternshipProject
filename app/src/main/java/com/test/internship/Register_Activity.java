fe1837655b1bd861be4a2625450141b33e921a17
LeeHuiJeong <55134095+LeeHuiJeong@users.noreply.github.com>
2020-06-09 00:15:13 +0900

    미니창 레이아웃

--- app/src/main/java/com/test/internship/Register_Activity.java
+++ app/src/main/java/com/test/internship/Register_Activity.java
@@ -1,10 +1,12 @@
 package com.test.internship;
 
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 
 
 import androidx.appcompat.app.AppCompatActivity;
