748b05a87e41599d1e3eaf80e238eec27723e0cf
LeeHuiJeong <55134095+LeeHuiJeong@users.noreply.github.com>
2020-06-18 07:20:15 +0900

    쓰플래쒸~

---
+++ app/src/main/java/com/test/internship/Splash.java
@@ -0,0 +1,29 @@
+package com.test.internship;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new splashHandler(), 30000);
+    }
+
+    private class splashHandler implements Runnable{
+        public void run(){
+            startActivity(new Intent(getApplication(), User.class));
+            Splash.this.finish();
+        }
+    }
+
+    @Override
+    // 초반 플래시 화면에서 넘어갈 때, 뒤로가기 버튼 못 누르게 함
+    public void onBackPressed() { }
+}
